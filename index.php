dc6844898a512cbc2e5dd48d028a95fc506cc900
chrolss <chrolss@kth.se@>
2018-05-03 20:57:49 +0200

    initial

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+header('Content-Type: application/json');
+
+$method = $_SERVER['REQUEST_METHOD'];
+
+// Process only if method is POST from Dialogflow
+if ($method == "POST"){
+  $requestBody = file_get_contents('php://input');
+  $json = json_decode($requestBody);
+
+  $text = $json->result->resolvedQuery;
+  $text = strtoupper($text);
+
+
+  $result = ["tjenare"];
+
+  $response = new \stdClass();
+  $response->speech = $result;
+  $response->displayText = $result;
+  $response->source = "webhook";
+  echo json_encode($response);
+} else {
+  echo "Not allowed";
+}
+
+?>
